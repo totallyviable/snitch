5737820cf4c90f6393b1f01c6cd000d820b85923
jake <jacob.bijani@gmail.com>
2016-01-20 14:18:14 -0500

    simplify uptime command

--- bot.js
+++ bot.js
@@ -202,10 +202,27 @@ controller.hears(['shutdown'],'direct_message,direct_mention,mention',function(b
 });
 
 
-controller.hears(['uptime','identify yourself','who are you','what is your name'],'direct_message,direct_mention,mention',function(bot, message) {
+controller.hears(['uptime'],'direct_message,direct_mention,mention',function(bot, message) {
 
     var hostname = os.hostname();
-    var uptime = formatUptime(process.uptime());
+    var uptime = process.uptime();
+    var unit = 'second';
+    
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
 
     bot.reply(message,':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '.');
 });
@@ -281,20 +298,3 @@ function emoji_reaction_error_callback(err, res) {
     }
 }
 
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
